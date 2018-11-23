4cd5908dd85b0d985e56f121e8a9efd86e14ce9d
Doz Lau <liuxiaoliang@vlifepaper.com>
2018-11-23 09:52:12 +0800

    检测结果页面选择时间后的UI完成

--- pages/result/result.js
+++ pages/result/result.js
@@ -2,18 +2,13 @@
 import * as tools from "../../utils/tools";
 
 Page({
-
-    /**
-     * 页面的初始数据
-     */
     data: {
         dateText: {},
-        backgroundColor: '#3E3E3E'
+        backgroundColor: '#3E3E3E',
+        isFinishChose: false,
+        cardTitle: '请问本次是在什么状态下检测的？'
     },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
     onLoad: function (options) {
         let date = tools.createDateAndTime(new Date());
         let dateText = date.date + '\n' + date.time;
@@ -26,52 +21,10 @@ Page({
         })
     },
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
+    clickChoose: function (e) {
+        console.log(e.currentTarget.dataset.index);
+        this.setData({
+            isFinishChose: true
+        })
     }
 })
