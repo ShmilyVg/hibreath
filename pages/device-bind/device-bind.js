f5649f3ccd5854ef5b94000298ee317b33929353
liubiao <liubiao@vlifepaper.com>
2018-11-23 11:54:17 +0800

    策略模式修改设备绑定界面UI

--- pages/device-bind/device-bind.js
+++ pages/device-bind/device-bind.js
@@ -5,19 +5,29 @@ Page({
      * 页面的初始数据
      */
     data: {
-        resultText: {color: '#979797', text: '正在寻找您的设备\n请将设备开机并靠近手机'},
-        lookingText: {color: '#979797', text: '正在寻找您的设备\n请将设备开机并靠近手机'},
-        findText: {color: '#FE5E01', text: '已找到您的设备\n短按设备上的按键确认绑定'},
-        failText: {color: '#979797',text:'绑定失败，请检查后重试'},
-        failRemindList:[
-            {text:'您的手机未开启蓝牙'},
-            {text:'您的手机未授权微信获取定位权限'},
-            {text:'您的设备正在被其他人使用'},
-            {text:'您未在设备上短按按键确认绑定'},
+        result: {color: '#979797', text: '正在寻找您的设备\n请将设备开机并靠近手机'},
+
+        failRemindList: [
+            {text: '您的手机未开启蓝牙'},
+            {text: '您的手机未授权微信获取定位权限'},
+            {text: '您的设备正在被其他人使用'},
+            {text: '您未在设备上短按按键确认绑定'},
         ]
     },
 
-
+    state: {
+        looking: {color: '#979797', text: '正在寻找您的设备\n请将设备开机并靠近手机'},
+        found: {color: '#FE5E01', text: '已找到您的设备\n短按设备上的按键确认绑定'},
+        failed: {color: '#979797', text: '绑定失败，请检查后重试'},
+    },
+    getResultState({state}) {
+        return this.state[state];
+    },
+    showResult({state}) {
+        this.setData({
+            result: this.getResultState({state})
+        })
+    },
     /**
      * 生命周期函数--监听页面加载
      */
