23faa2b9b74d94cd7b8a5ee7a23184da5ce985eb
Doz Lau <liuxiaoliang@vlifepaper.com>
2018-11-26 12:11:05 +0800

    检测结果页面逻辑基本完成

--- modules/network/protocol.js
+++ modules/network/protocol.js
@@ -23,6 +23,8 @@ export default class Protocol {
     }
 
     static getAnalysisFetch({dataValue, situation}) {
+        // Network.request({url: 'breathData/add', data: {dataValue, situation}});
         return Network.request({url: 'analysis/fetch', data: {dataValue, situation}})
     }
+
 }
--- pages/result/result.js
+++ pages/result/result.js
@@ -1,6 +1,7 @@
 // pages/result/result.js
 import * as tools from "../../utils/tools";
-import protocol from "../../modules/network/protocol";
+import Protocol from "../../modules/network/protocol";
+import toast from "../../view/toast";
 
 Page({
     data: {
@@ -9,55 +10,73 @@ Page({
         viewUnscramble: false,
         isChose: false,
         cardTitle: '请问本次是在什么状态下检测的？',
-        score: 99,
+        score: 60,
     },
 
     onLoad: function (options) {
-        let list = protocol.getAnalysisSituation();
+        let that = this;
+        Protocol.getAnalysisSituation().then(data => {
+            let list = data.result.list;
+            that.setData({
+                list: list
+            })
+        });
         let date = tools.createDateAndTime(new Date());
         let dateText = date.date + '\n' + date.time;
-        let backgroundColor = '#000000';
+        let backgroundColor = '';
         let score = this.data.score;
-        if (score < 6) {
+        if (score <= 5) {
             backgroundColor = '#3E3E3E'
-        } else if (score > 5 && score < 31) {
+        } else if (score > 5 && score <= 30) {
             backgroundColor = '#FF7C00'
-        } else if (score > 30 && score < 81) {
+        } else if (score > 30 && score <= 80) {
             backgroundColor = '#FF5E00'
         } else if (score > 80) {
             backgroundColor = '#E64D3D'
         }
-        this.setData({
-            list: list,
+        that.setData({
             dateText: dateText,
             backgroundColor: backgroundColor,
-            score:score
+            score: score
         });
         wx.setNavigationBarColor({
             frontColor: '#ffffff',
             backgroundColor: backgroundColor
-        })
+        });
     },
 
     clickChoose: function (e) {
-        let index = e.currentTarget.dataset.index;
-        let list = this.data.list;
-        for (let i in list) {
-            list[i]['isChose'] = index == list[i]['key'];
-            this.data.cardTitle = list[i]['text_zh'];
+        if (e.currentTarget.dataset.index) {
+            let index = e.currentTarget.dataset.index;
+            let list = this.data.list;
+            for (let i in list) {
+                list[i]['isChose'] = index == list[i]['key'];
+                this.data.cardTitle = list[i]['text_zh'];
+            }
+            this.setData({
+                list: list,
+                isChose: true,
+                index: index
+            })
         }
-        this.setData({
-            list: list,
-            isChose: true,
-            index: index
-        })
     },
 
     clickBtn: function () {
-        let content = protocol.getAnalysisFetch({dataValue: this.data.score, situation: this.data.index});
-        this.setData({
-            viewUnscramble: true,
-            cardTitle: this.data.cardTitle
-        })
+        if (!this.data.index) {
+            return;
+        }
+        let that = this;
+        toast.showLoading();
+        Protocol.getAnalysisFetch(
+            {dataValue: this.data.score, situation: parseInt(this.data.index)}
+        ).then(data => {
+            let description = data.result.description;
+            that.setData({
+                description: description,
+                viewUnscramble: true,
+                cardTitle: this.data.cardTitle
+            });
+            toast.hiddenLoading();
+        });
     }
 })
