04d0ab1cf399d2d51bc4b58dd59aaeae9277106e
liubiao <liubiao@vlifepaper.com>
2018-11-26 11:36:32 +0800

    重构注册登录部分

--- modules/network/login.js
+++ modules/network/login.js
@@ -1,53 +1,55 @@
 import Network from "./network";
 import WXDialog from "../../view/dialog";
 
-
+let _needRegister = false;
 export default class Login {
     static doLogin() {
         return new Promise((resolve, reject) =>
             this._wxLogin().then(res => {
                     const token = wx.getStorageSync('token');
-                    Network.request({url: 'account/login', data: {js_code: res.code, token}, requestWithoutLogin: true})
-                        .then(data => {
-                            this._setToken({data});
-                            Network.resendAll();
-                            resolve();
-                        })
-                        .catch(res => {
-                            console.log('login failed', res);
-                            if (res.data) {
-                                const {data: {code}} = res;
-                                if (code === 2) {
-                                    console.log('未注册，请先注册');
-                                    return;
-                                }
-                            }
-                            WXDialog.showDialog({title: '糟糕', content: '抱歉，目前小程序无法登录，请稍后重试'});
-                        })
+                    return Network.request({
+                        url: 'account/login',
+                        data: {js_code: res.code, token},
+                        requestWithoutLogin: true
+                    })
                 }
-            )
+            ).then(data => {
+                this._setToken({data});
+                Network.resendAll();
+                resolve();
+            }).catch(res => {
+                console.log('login failed', res);
+                if (res.data) {
+                    const {data: {code}} = res;
+                    if (code === 2) {
+                        console.log('未注册，请先注册');
+                        _needRegister = true;
+                        return;
+                    }
+                }
+                reject(res);
+                WXDialog.showDialog({title: '糟糕', content: '抱歉，目前小程序无法登录，请稍后重试'});
+            })
         );
     }
 
     static doRegister({userInfo, encryptedData, iv}) {
         return new Promise((resolve, reject) =>
-            this._wxLogin().then(res => {
+            _needRegister ? this._wxLogin().then(res => {
                 const {code} = res;
-                Network.request({
+                return Network.request({
                     url: 'account/register',
                     data: {code, encrypted_data: encryptedData, iv},
                     requestWithoutLogin: true
                 })
-                    .then(data => {
-                        this._setToken({data});
-                        Network.resendAll();
-                        resolve();
-                    })
-                    .catch(res => {
-                        console.log('register failed:', res);
-                        reject(res);
-                    })
-            })
+            }).then(data => {
+                this._setToken({data});
+                Network.resendAll();
+                resolve();
+            }).catch(res => {
+                console.log('register failed:', res);
+                reject(res);
+            }) : resolve()
         )
 
     }
@@ -62,13 +64,6 @@ export default class Login {
             })
         );
     }
-
-    // static _success({userInfo: {nickName, avatarUrl}}) {
-    //     const app = getApp();
-    //     app.globalData.userInfo.headUrl = avatarUrl;
-    //     app.globalData.userInfo.nickName = nickName;
-    //     console.log('全局变量', app.globalData);
-    // }
 
     static _setToken({data: {result: {jsessionid}}}) {
         Network.setToken({token: jsessionid});
