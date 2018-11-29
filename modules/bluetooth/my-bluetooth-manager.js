d7c26ce22987177b64545613488903e07f27fc6d
liubiao <liubiao@vlifepaper.com>
2018-11-29 18:00:18 +0800

    设置四种新的设备状态

--- modules/bluetooth/my-bluetooth-manager.js
+++ modules/bluetooth/my-bluetooth-manager.js
@@ -6,6 +6,10 @@ export default class MyBlueToothManager extends SimpleBlueToothImp {
     static DISCONNECT = BaseBlueToothImp.DISCONNECT;
     static CONNECTING = BaseBlueToothImp.CONNECTING;
     static CONNECTED = BaseBlueToothImp.CONNECTED;
+    static PRE_HOT_START = 'pre_hot_start';//开始预热状态
+    static PRE_HOT_FINISH_AND_START_BREATH = 'pre_hot_finish_and_start_breath';//预热完成开始吹气
+    static BREATH_FINISH_AND_SUCCESS = 'breath_finish_and_success';//吹气完成返回结果
+    static BREATH_RESTART = 'breath_restart';//重新吹气
 
     constructor() {
         super();
