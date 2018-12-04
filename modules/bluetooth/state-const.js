276fb376ad0102c3b40e38316fb7d520b70985ce
liubiao <liubiao@vlifepaper.com>
2018-12-04 11:35:21 +0800

    设备未绑定状态提升至全局更新

--- modules/bluetooth/state-const.js
+++ modules/bluetooth/state-const.js
@@ -1,6 +1,7 @@
 import BaseBlueToothImp from "../../libs/bluetooth/base/base-bluetooth-imp";
 
 export default class BlueToothState {
+    static UNBIND = 'unbind';
     static UNAVAILABLE = BaseBlueToothImp.UNAVAILABLE;
     static DISCONNECT = BaseBlueToothImp.DISCONNECT;
     static CONNECTING = BaseBlueToothImp.CONNECTING;
