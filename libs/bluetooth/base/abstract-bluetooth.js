161fbe938c780185106ff1005ac2ff3555ff0a36
liubiao <liubiao@vlifepaper.com>
2018-12-14 18:21:53 +0800

    解绑问题

--- libs/bluetooth/base/abstract-bluetooth.js
+++ libs/bluetooth/base/abstract-bluetooth.js
@@ -104,6 +104,13 @@ export default class AbstractBlueTooth {
         return this.closeAdapter().finally(() => {
             wx.removeStorageSync('deviceId');
             this._deviceId = '';
+            this._isOpenAdapter = false;
+            this._isStartDiscovery = false;
+            this._isActiveCloseBLE = false;
+            this._isConnected = false;
+            this._deviceId = '';
+            this._serviceId = '';
+            this._characteristicId = '';
         });
     }
 
