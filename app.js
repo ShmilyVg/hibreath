aa52afe00c57c0daacc42c508e6312535cd7dc3a
liubiao <liubiao@vlifepaper.com>
2018-12-19 10:31:20 +0800

    呼吸酮与服务器同步数据

--- app.js
+++ app.js
@@ -3,28 +3,31 @@ import 'libs/adapter';
 import 'modules/network/update';
 import {common} from "./libs/bluetooth/app/common";
 import HiBreathBlueToothManager from "./modules/bluetooth/hi-breath-bluetooth-manager";
+import {ProtocolState} from "./modules/bluetooth/bluetooth-state";
+import Protocol from "./modules/network/protocol";
+
 
 App({
     onLaunch(options) {
         let records = [];
         this.setCommonBLEListener({
             commonAppReceiveDataListener: ({finalResult, state}) => {
-                // if (ProtocolState.QUERY_DATA_ING === state.protocolState) {
-                //     const {length, isEat, timestamp} = finalResult;
-                //     if (records.length < length) {
-                //         records.push({state: isEat ? 1 : 0, timestamp});
-                //     } else {
-                //         Protocol.postMedicalRecordSave({records}).then(data => {
-                //             //TODO 向设备回复成功
-                //             this.bLEManager.sendQueryDataSuccessProtocol();
-                //         }).catch(res => {
-                //             console.log(res, '同步数据失败');
-                //         }).finally(() => records = []);
-                //     }
-                //
-                // } else {
-                //     this.appReceiveDataListener && this.appReceiveDataListener({finalResult, state});
-                // }
+                if (ProtocolState.QUERY_DATA_ING === state.protocolState) {
+                    const {length, isEat, timestamp} = finalResult;
+                    if (records.length < length) {
+                        records.push({state: isEat ? 1 : 0, timestamp});
+                    } else {
+                        Protocol.postMedicalRecordSave({records}).then(data => {
+                            //TODO 向设备回复成功
+                            this.bLEManager.sendQueryDataSuccessProtocol();
+                        }).catch(res => {
+                            console.log(res, '同步数据失败');
+                        }).finally(() => records = []);
+                    }
+
+                } else {
+                    this.appReceiveDataListener && this.appReceiveDataListener({finalResult, state});
+                }
             },
             commonAppBLEStateListener: ({state}) => {
                 this.appBLEStateListener && this.appBLEStateListener({state});
