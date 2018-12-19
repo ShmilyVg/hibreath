650011c299f0751af926b95d3853d9702131f1a2
李小云 <lixiaoyun@qiland.com>
2018-12-19 16:34:31 +0800

    减脂攻略修改了第四页

--- pages/set-info/set-info.js
+++ pages/set-info/set-info.js
@@ -11,7 +11,9 @@ Page({
         page: 1,
         title: ['你的性别是？', '你的出生日期？', '你的身高？', '你的体脂情况？'],
         text: ['告诉我们关于你的事，\n让我帮你获得更适合的健康方案', '我们会针对不同的年龄为你定制相应的健康方案', '', '根据图片估算一下你的体脂含量吧'],
-        page4Item: ['3-4%', '6-7%', '10-12%', '15%', '20%', '25%', '30%', '35%', '40%']
+        page4MenItem: ['3-4%', '6-7%', '10-12%', '15%', '20%', '25%', '30%', '35%', '40%'],
+        page4WomenItem: ['10-12%', '15-17%', '20-22%', '25%', '30%', '35%', '40%', '45%', '50%'],
+        itemBackgroundColor: '#656565',
     },
     onLoad: function (options) {
         let timeS = tools.createDateAndTime(Date.parse(new Date()));
@@ -86,6 +88,13 @@ Page({
         this.setData({
             'info.weight': e.detail.value
         })
+    },
+
+    page4ItemClick(e){
+        let index = e.currentTarget.dataset.index;
+        this.setData( {
+           choseIndex: index
+        } );
     }
 
 })
