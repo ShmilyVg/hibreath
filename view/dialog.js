7487cdf593556c3e99e20bdcdc02d323d40a62c8
liubiao <liubiao@vlifepaper.com>
2018-12-21 16:23:12 +0800

    通用View组件依赖

--- view/dialog.js
+++ view/dialog.js
@@ -1,22 +1,3 @@
-export default class WXDialog {
-    static showDialog({title='', content, showCancel = false, cancelText = '取消', confirmText = '确定', confirmEvent, cancelEvent}) {
-        wx.showModal({
-            title,
-            content,
-            showCancel,
-            cancelText,
-            confirmText,
-            success: res => {
-                if (res.confirm) {
-                    confirmEvent && confirmEvent();
-                } else {
-                    cancelEvent && cancelEvent();
-                }
-            },
-            fail:res=>{
-                console.log(res);
-            },
-            confirmColor: '#D16730',
-        })
-    }
-}
+import {WXDialog} from "heheda-common-view";
+
+export default WXDialog;
