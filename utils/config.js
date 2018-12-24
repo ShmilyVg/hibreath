438c0f4c50d0864239efd8e204028e7919be1137
liubiao <liubiao@vlifepaper.com>
2018-12-24 17:33:45 +0800

    配置url

--- utils/config.js
+++ utils/config.js
@@ -2,7 +2,7 @@ import {NetworkConfig} from "heheda-network";
 
 const PostUrl = 'https://backend.stage.hipee.cn/hipee-web-hibreath/';
 
-NetworkConfig.setConfig({url: PostUrl});
+NetworkConfig.setConfig({postUrl: PostUrl});
 export {
     PostUrl,
 };
