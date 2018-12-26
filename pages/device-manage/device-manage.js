ea4538be152ad1bc13b384018859a05b6d2cfeb2
Doz Lau <liuxiaoliang@vlifepaper.com>
2018-12-26 11:45:46 +0800

    增加toast显示隐藏

--- pages/device-manage/device-manage.js
+++ pages/device-manage/device-manage.js
@@ -1,6 +1,7 @@
 // pages/device-manage/device-manage.js
 import Protocol from "../../modules/network/protocol";
 import HiNavigator from "../../navigator/hi-navigator";
+import {Toast} from "heheda-common-view";
 
 Page({
     data: {
@@ -8,20 +9,22 @@ Page({
     },
 
     onLoad() {
+        Toast.showLoading();
         Protocol.getDeviceBindInfo().then(data => {
             const deviceInfo = data.result;
             this.setData({
                 deviceId: deviceInfo.deviceId
             })
-        })
+        }).finally(() => Toast.hiddenLoading());
 
     },
     unbindDevice() {
+        Toast.showLoading();
         Protocol.postDeviceUnbind({deviceId: this.data.deviceId}).then(data => {
             getApp().getBLEManager().clearConnectedBLE();
             HiNavigator.reLaunch({url: '/pages/index/index'});
         }).catch(res => {
             console.log(res);
-        })
+        }).finally(() => Toast.hiddenLoading());
     }
 })
