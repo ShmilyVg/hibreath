dfab276a98ae64d968a92923d4e9f7406f874170
liubiao <liubiao@vlifepaper.com>
2018-12-27 11:58:03 +0800

    小数位只保留两位

--- modules/bluetooth/hi-breath-bluetooth-protocol.js
+++ modules/bluetooth/hi-breath-bluetooth-protocol.js
@@ -27,7 +27,7 @@ export default class HiBreathBlueToothProtocol extends HiBlueToothProtocol {
                 super.sendData({command: '0x37'});
                 const timestamp = HexTools.hexArrayToNum(dataArray.slice(0, 4));
                 const dou = HexTools.hexArrayToNum(dataArray.slice(5, 6));
-                const result = HexTools.hexArrayToNum(dataArray.slice(4, 5)) + '.' + parseInt(dou >= 100 ? dou / 10 : dou);
+                const result = HexTools.hexArrayToNum(dataArray.slice(4, 5)) + '.' + Math.floor(dou >= 100 ? dou / 10 : dou);
                 return {state: ProtocolState.BREATH_RESULT, dataAfterProtocol: {result, timestamp}};
             },
             '0x38': () => {
