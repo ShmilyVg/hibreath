2664cff5e33673a3bd0f4c09c90cb83045b3bcc8
liubiao <liubiao@vlifepaper.com>
2019-07-10 10:43:43 +0800

    呼吸酮名称

--- modules/bluetooth/hi-breath-bluetooth-manager.js
+++ modules/bluetooth/hi-breath-bluetooth-manager.js
@@ -9,7 +9,7 @@ export default class HiBreathBlueToothManager extends HiBlueToothManager {
         this.setUUIDs({
             services: ['0000180A-0000-1000-8000-00805F9B34FB'],
             hiServiceUUID: '6E400001-B5A3-F393-E0A9-E50E24DCCA9E',
-            hiDeviceName: 'PB2-'
+            hiDeviceName: 'HiKeto-'
         });
     }
 
