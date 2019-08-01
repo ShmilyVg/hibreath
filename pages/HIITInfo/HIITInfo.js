8dd8cd58ef430232fe57c5252412d2bcd7aa47de
张浩玉 <826514335@qq.com>
2019-08-01 14:57:48 +0800

    HIIT视频页面

--- pages/HIITInfo/HIITInfo.js
+++ pages/HIITInfo/HIITInfo.js
@@ -5,6 +5,7 @@ import Protocol from "../../modules/network/protocol";
 Page({
 
     data: {
+        videoUrl:"http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400",
         confirmText: '好哒',
         cancelText: '取消',
         tintColor: 'color:#00a48f',
