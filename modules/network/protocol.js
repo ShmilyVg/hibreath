bf13f7a9125a4d8536d862e9cd58b5b52b390195
Doz Lau <liuxiaoliang@vlifepaper.com>
2019-09-19 11:28:11 +0800

    新手引导除切图外UI完成

--- modules/network/protocol.js
+++ modules/network/protocol.js
@@ -29,9 +29,10 @@ export default class Protocol {
     static getBreathDataAdd({dataValue, situation}) {
         return Network.request({url: 'breathData/add', data: {dataValue, situation}});
     }
-   /* static getBreathDataAdd({dataValue}) {
-        return Network.request({url: 'breathData/add', data: {dataValue}});
-    }*/
+
+    /* static getBreathDataAdd({dataValue}) {
+         return Network.request({url: 'breathData/add', data: {dataValue}});
+     }*/
     static getBreathDataList({page, pageSize = 15}) {
         return Network.request({url: 'breathData/list', data: {page, pageSize}})
     }
@@ -39,6 +40,7 @@ export default class Protocol {
     static getAnalysisNotes() {
         return Network.request({url: 'analysis/notes'})
     }
+
     //设备绑定相关
     static getDeviceBindInfo() {
         return Network.request({url: 'device/bind/info'})
@@ -47,6 +49,7 @@ export default class Protocol {
     static postDeviceBind({deviceId, mac}) {
         return Network.request({url: 'device/bind', data: {deviceId, mac}});
     }
+
     //设备绑定相关
     static postDeviceUnbind({deviceId}) {
         return Network.request({url: 'device/unbind', data: {deviceId}});
@@ -55,6 +58,7 @@ export default class Protocol {
     static postBreathDataSetSituation({id, situation}) {
         return Network.request({url: 'breathData/setSituation', data: {id, situation}});
     }
+
     //上传身体评估信息
     static postBreathPlanAnalysis(data) {
         return Network.request({url: 'breathPlan/analysis', data: data})
@@ -71,10 +75,12 @@ export default class Protocol {
     static postPhysical(data) {
         return Network.request({url: 'breathData/sync', data: data});
     }
+
     //个人中心--体重体脂记录-修改
     static postSetBMIInfo(data) {
         return Network.request({url: 'breathData/sync', data: data});
     }
+
     //获取PPM 脂肪燃烧速度对应等级 步行-骑行-汽车等
     static postSetGradeInfo(data) {
         return Network.request({url: '/breathData/info', data: data});
@@ -94,16 +100,28 @@ export default class Protocol {
     static postCookInfo({Id}) {
         return Network.request({url: 'breathData/sync', data: {Id}});
     }
+
     //今日身体情况记录--体重&体脂
     static postTodayBMI(data) {
         return Network.request({url: 'breathData/list', data: data})
     }
+
     //获取减脂方案目标
     static getTargetNmuber(data) {
         return Network.request({url: 'breathData/sync', data: data});
     }
+
     //HIIT视频
     static postHIIT(data) {
         return Network.request({url: 'breathData/list', data: data})
+    }
+
+    // 减脂目标
+    static postSettingsGoals() {
+        return Network.request({url: 'settings/goals'})
+    }
+
+    static postMembersPut(data) {
+        return Network.request({url: 'members/put', data})
     }
 }
--- pages/set-info/set-info.js
+++ pages/set-info/set-info.js
@@ -8,7 +8,7 @@ import WXDialog from "../../view/dialog";
 Page({
 
     data: {
-        isexact: false,//是否准确测过体脂率
+        noMeasure: false,//没有准确测过体脂率
         sexBox: [
             {image: 'man', text: '男士', isChose: false},
             {image: 'woman', text: '女士', isChose: false}
@@ -16,102 +16,65 @@ Page({
         currentDate: '2018-12-19',
         page: 1,
         choseIndex: "3",
-        title: ['减值目标', '体重目标', '性别', '出生日期', '身高', '体脂率', '您的三餐选择', '选择一套方案'],
+        title: ['减脂目标', '性别', '出生日期', '身高体重', '体脂率', '您的三餐选择', '推荐目标体重', '选择一套方案'],
         page4MenItem: ['3-4%', '6-7%', '10-12%', '15%', '20%', '25%', '30%', '35%', '40%'],
         page4WomenItem: ['10-12%', '15-17%', '20-22%', '25%', '30%', '35%', '40%', '45%', '50%'],
         itemBackgroundColor: '#656565',
+        birth: ['1980', '01', '01'],
         meals: [
-            {text: '外卖为主', isChose: false},
-            {text: '外出就餐为主', isChose: false},
-            {text: '单位食堂为主', isChose: false}
+            {text: '外卖为主', isChose: false, en: 'waimai'},
+            {text: '外出就餐为主', isChose: false, en: 'waichu'},
+            {text: '单位食堂为主', isChose: false, en: 'shitang'}
         ]
     },
-    onLoad: function (options) {
+
+    async onLoad() {
         //截止日期范围
         const {year, month, day} = tools.createDateAndTime(Date.parse(new Date()));
-        let currentDate = `${year}-${month}-${day}`;
-        this.setData({currentDate});
-        // Protocol.postPhysical().then(data => {
-        //     let dataInfo = data.result
-        //     if (dataInfo.info.birthday !== "") {
-        //         this.setData({
-        //             birthday: dataInfo.info.birthday
-        //         })
-        //     } else {
-        //         this.setData({
-        //             birthday: '1980-01-01',
-        //         })
-        //     }
-        //     if (dataInfo.info.height !== "") {
-        //         this.setData({
-        //             height: dataInfo.info.height
-        //         })
-        //     } else {
-        //         this.setData({
-        //             height: '175',
-        //         })
-        //     }
-        //     if (dataInfo.info.weight !== "") {
-        //         this.setData({
-        //             weight: dataInfo.info.weight
-        //         })
-        //     } else {
-        //         this.setData({
-        //             weight: '70',
-        //         })
-        //     }
-        //     if (dataInfo.info.bodyFatRate !== "") {
-        //         this.setData({
-        //             bodyFatRate: dataInfo.info.bodyFatRate
-        //         })
-        //     } else {
-        //         this.setData({
-        //             bodyFatRate: '25%',
-        //         })
-        //     }
-        //     //获取上次填写性别 出生日期等记录
-        // })
-        let info = {
-            birthday: '1980-01-01',
-            sex: 1,
-            height: '11',
-            weight: '',
-            bodyFatRate: '12.4',
-        };
-        /* for(var i=0;i<this.data.sexBox.length;i++){
-             if(this.data.info.sex ==this.data.sexBox[i]. image){
-
-             }
-         }*/
-
-        this.setData({
-            info: info
-        })
+        const currentDate = `${year}-${month}-${day}`;
+        const {result: {list: goals}} = await Protocol.postSettingsGoals();
+        const {result: {finishedGuide}} = await Protocol.getAccountInfo();
+        // let info = {
+        //     goalDesc: '',
+        //     sex: 1,
+        //     sexStr: 'man',
+        //     birthday: '1980-01-01',
+        //     height: 180,
+        //     weight: 0,
+        //     bodyFatRate: '12.4',
+        //     weightGoal: 0,
+        // };
+        this.setData({currentDate, goals})
     },
 
     continue() {
         const info = this.data.info;
         switch (this.data.page) {
             case 1:
+                if (this.objIsEmpty(info.goalDesc)) {
+                    toast.warn('请填写目标');
+                    return;
+                }
                 break;
             case 2:
-                break;
-            case 3:
                 if (this.objIsEmpty(info.sex)) {
                     toast.warn('请选择性别');
                     return;
                 }
                 break;
-            case 4:
+            case 3:
                 break;
-            case 5:
+            case 4:
                 if (this.objIsEmpty(info.height)) {
                     toast.warn('请填写身高');
                     return;
+                } else if (this.objIsEmpty(info.weight)) {
+                    toast.warn('请填写体重');
+                    return;
                 }
                 break;
-            case 6:
-                if (this.data.isexact) {
+            case 5:
+                if (this.data.noMeasure) {
                     if (this.objIsEmpty(this.data.choseIndex)) {
                         toast.warn('请选择图片');
                         return;
@@ -142,12 +105,24 @@ Page({
 
                 }
                 break;
+            case 6:
+                let isChoseMeals = false;
+                this.data.meals.forEach(value => {
+                    if (value.isChose) {
+                        isChoseMeals = true;
+                    }
+                });
+                if (!isChoseMeals) {
+                    toast.warn('请选择三餐');
+                    return
+                }
+                break;
             case 7:
                 break;
             case 8:
-                Protocol.postBreathPlanAnalysis(info).then(data => {
-                    this.setisFirst();
-                });
+                // Protocol.postBreathPlanAnalysis(info).then(data => {
+                //     this.setisFirst();
+                // });
                 return;
         }
         this.setData({
@@ -169,7 +144,12 @@ Page({
         })
     },
 
-    chooseSex(e) {
+    //BIND
+    bindInputGoal(e) {
+        this.setData({'info.goalDesc': e.detail.value})
+    },
+
+    bindTapSex(e) {
         let choseIndex = e.currentTarget.dataset.index;
         this.data.sexBox.map((value, index) => {
             value.isChose = choseIndex == index;
@@ -187,99 +167,54 @@ Page({
         })
     },
 
-    clickMeals(e) {
-        let choseIndex = e.currentTarget.dataset.index;
-        console.log(choseIndex);
-        this.data.meals.map((value, index) => {
-            value.isChose = choseIndex == index
+    bindChangeBirth(e) {
+        const value = e.detail.value;
+        const birthArr = value.split("-");
+        this.setData({
+            'info.birthday': value,
+            birth: birthArr
+        })
+    },
+
+    bindInputHeight(e) {
+        const height = e.detail.value;
+        let weightGoal = (height / 100) * (height / 100) * 21;
+        weightGoal = weightGoal.toFixed(1);
+        this.setData({
+            'info.height': height,
+            'info.weightGoal': weightGoal
         });
-        this.setData({meals: this.data.meals})
     },
 
-    setisFirst() {
-        this.setData({
-            'isFirst': false
-        })
+    bindInputWeight(e) {
+        this.setData({'info.weight': e.detail.value});
     },
 
-    bindTarget(e) {
-        console.log(e.detail.value);
+    bindInputWeightGoal(e) {
+        this.setData({'info.weightGoal': e.detail.value});
     },
 
-    bindBirthChange(e) {
-        let value = e.detail.value;
-        this.setData({
-            'info.birthday': value
-        })
+    bindInputExact(e) {
+        this.setData({'info.bodyFatRate': e.detail.value})
     },
 
-    bindHeightInput(e) {
-        this.setData({
-            'info.height': e.detail.value
-        })
-        console.log(typeof(e.detail.value), "89888")
+    bindTapMeals(e) {
+        let choseIndex = e.currentTarget.dataset.index;
+        this.data.meals.map((value, index) => {
+            value.isChose = choseIndex == index;
+        });
+        const en = this.data.meals[choseIndex].en;
+        this.setData({meals: this.data.meals, 'info.mealType': en})
     },
 
-    bindWeightInput(e) {
-        this.setData({
-            'info.weight': e.detail.value
-        })
-        console.log("test,", this.info.weight)
-    },
-
-    /* bindWeightInput(e) {
-         let that =this;
-         let dataset =e.currentTarget.dataset;
-         console.log(dataset,"dataset")
-         let value = e.detail.value;
-         let nameTest =dataset.nameT;
-         that.data[nameTest]=value;
-         this.setData({
-             'info.weight':  that.data[nameTest]
-         })
-         console.log("test,",that.data[nameTest])
-     },*/
-
-
-    //填写体脂率
-    bindExactInput(e) {
-        var BMINumber;
-        BMINumber = e.detail.value;
-        console.log(BMINumber);
-        this.setData({
-            'info.bodyFatRate': BMINumber
-        })
-        /* if (BMINumber.toString().split(".")[1].length<=1 && Number(BMINumber)<100) { //正则验证，小数点后不能大于1位数字
-             this.setData({
-                 'info.bodyFatRate': BMINumber+"%"
-             })
-         } else {
-             WXDialog.showDialog({
-                 title: '小贴士', content:"请输入正确的体脂率，体脂率范围1%-100%", confirmText: '我知道了', confirmEvent: () => {
-
-                 }
-             });
-         };
- */
-    },
-
-    page4ItemClick(e) {
+    bindTapExactClick(e) {
         let index = e.currentTarget.dataset.index;
         this.setData({
             choseIndex: index
         });
     },
 
-    IntoExact() {
-        this.setData({
-            isexact: false
-        });
-    },
-
-    IntoNoExact() {
-        this.setData({
-            isexact: true
-        });
+    bindTapSwitchExact() {
+        this.setData({noMeasure: !this.data.noMeasure});
     }
-
 })
