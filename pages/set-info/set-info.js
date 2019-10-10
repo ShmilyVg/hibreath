aa2b6cac533f0ed2bd1f98906ec522f2b256f099
张浩玉 <826514335@qq.com>
2019-10-10 10:01:50 +0800

    Fix：体重强制小数点后一位

--- pages/set-info/set-info.js
+++ pages/set-info/set-info.js
@@ -591,9 +591,10 @@ Page({
     },
     bindWeightInput(e){
         const weightNumber = e.detail.value.split(".");
-        console.log('eeeee',weightNumber)
-        if(weightNumber[1]>9){
-            return Number(e.detail.value).toFixed(1);
+        console.log('eeeee',weightNumber[1])
+        if(weightNumber[1]>9 ||weightNumber[1] === "0"){
+            //return Number(e.detail.value).toFixed(1);
+            return tools.subStringNum(e.detail.value)
         }
         if(weightNumber.length>2){
             return parseInt(e.detail.value);
