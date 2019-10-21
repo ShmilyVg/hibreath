bbf2a04dce4cd6a315696b01bd66a356361d354e
liubiao <liubiao@vlifepaper.com>
2019-10-21 10:58:10 +0800

    设置网页标题和颜色值

--- pages/food-ruler/food-ruler.js
+++ pages/food-ruler/food-ruler.js
@@ -10,7 +10,8 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad(options) {
-
+        wx.setNavigationBarTitle({title: '自由日饮食原则'});
+        wx.setNavigationBarColor({frontColor:'#ffffff', backgroundColor: '#171717'});
     },
 
 
