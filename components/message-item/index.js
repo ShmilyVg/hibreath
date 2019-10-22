a2469c88fcb0d437849ffa819818def4bd332f4b
liubiao <liubiao@vlifepaper.com>
2019-10-22 12:11:23 +0800

    完成圈子页面

---
+++ components/message-item/index.js
@@ -0,0 +1,39 @@
+import {WXDialog} from "heheda-common-view";
+import Protocol from "../../modules/network/protocol";
+import {previewImage} from "../../view/view";
+
+Component({
+    options: {
+        addGlobalClass: true,
+    },
+    properties: {
+        message: {
+            type: Object,
+            value: {taskId: "", imgUrls: ['', '', '',], desc: '', messageCreateTime: '', headUrl: '', nickname: ''}
+        }
+    },
+
+    data: {},
+    lifetimes: {
+        created() {
+
+        },
+        attached() {
+
+        },
+    },
+    methods: {
+        onMessageSettingEvent() {
+            WXDialog.showDialog({
+                content: '确定要删除此条动态吗？', showCancel: true, confirmEvent: async () => {
+                    // await Protocol.postDynamicDelete({id: this.dataId});
+                    this.triggerEvent('onMessageDeleteEvent', {taskId: this.data.message.taskId});
+                }
+            });
+        },
+        async onImagePreview(e) {
+            const {currentTarget: {dataset: {url: current}}} = e;
+            await previewImage({current, urls: this.data.message.imgUrls});
+        }
+    }
+});
--- pages/community/community.js
+++ pages/community/community.js
@@ -1,10 +1,23 @@
 // pages/community/Community.js
+import {showActionSheet} from "../../view/view";
+
 Page({
 
     /**
      * 页面的初始数据
      */
-    data: {},
+    data: {
+        dynamicList: [
+            {
+                "imgUrls": ["http://tmp/wx3092c4629e38d21e.o6zAJs-ZTh1KE_1mWKYSo5jiADbc.qNpkLcR0ytCF8a0984514410a7da5719368bb39d1a24.png", "http://tmp/wx3092c4629e38d21e.o6zAJs-ZTh1KE_1mWKYSo5jiADbc.xF0JVgK6QAdK9c45a6b96cb01d63e8e3a743821b4107.png"],
+                "headUrl": "https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTKcfufWYfm64jCLxMoh21yta3dlGRCd8MPvrP7PLeJiaj0Gkh3AvwJFqg65oX2nnQuCUktFIuvzQ1g/132",
+                "nickname": "呵呵哒",
+                "taskId": "14852",
+                "createTimestamp": 1571646527466,
+                "desc": "2"
+            }
+        ]
+    },
 
     /**
      * 生命周期函数--监听页面加载
@@ -12,10 +25,28 @@ Page({
     onLoad(options) {
 
     },
-    onCommunitySettingClickEvent() {
-        console.log('点击了设置');
-    },
+    async onCommunitySettingClickEvent() {
+        try {
+            switch (await showActionSheet({itemList: ['更多圈子', '删除该圈子']})) {
+                case 0:
+                    break;
+                case 1:
+                    break;
+            }
+        } catch (e) {
+            console.warn(e);
+        }
 
+    },
+    onDynamicItemDeleteEvent({detail}) {
+        console.log(detail);
+        const {taskId} = detail, {dynamicList} = this.data;
+        const index = dynamicList.findIndex(item => item.taskId === taskId);
+        if (index !== -1) {
+            dynamicList.splice(index, 1);
+            this.setData({dynamicList});
+        }
+    },
 
     /**
      * 生命周期函数--监听页面显示
