59a709c9729303d38ba604f8ac47a099373a0eaf
张浩玉 <826514335@qq.com>
2019-11-16 15:46:03 +0800

    Fix:首页下拉刷新添加loading

--- pages/set-info/set-info.js
+++ pages/set-info/set-info.js
@@ -1116,10 +1116,13 @@ Page({
     /**
      * 页面相关事件处理函数--监听用户下拉动作
      */
-    onPullDownRefresh: function () {
-        wx.stopPullDownRefresh();
+    async onPullDownRefresh() {
+        Toast.showLoading();
         if(!this.data.showNewInfo&&!this.data.showGuide){
-            this.handleTasks()
+            await this.handleTasks()
         }
+
+        wx.stopPullDownRefresh();
+        Toast.hiddenLoading();
     }
 })
