dfbac6e0fd32dc884eaa0efa76bd46f0fb686617
sunyingying <sunyingying@fruitech.cn>
2019-12-06 16:21:38 +0800

    Style：无圈子时显示

--- pagesIndex/noticeList/noticeList.js
+++ pagesIndex/noticeList/noticeList.js
@@ -1,19 +1,80 @@
 // pagesIndex/noticeList/noticeList.js
+import Protocol from "../../modules/network/protocol";
+import HiNavigator from "../../navigator/hi-navigator";
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    groupId: 203,
+    MessageDetailId:''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.postDynamicNotice();
   },
+  
+  
+  async postDynamicNotice() {
+    const { result:{list:list} } = await Protocol.postDynamicNotice({ groupId: this.data.groupId });
+    console.log(list)
+    const dataList = list.map(item => {
+      return { ...item, timeDiff: this.getDateDiff(item.timeDiff) };
+    })
+    this.setData({
+      list: dataList
+    })
+  },
+  toMessageDetail: function (e) {
+    this.data.MessageDetailId = e.currentTarget.dataset.index;
+    HiNavigator.navigateToMessageDetail({ messageId: this.data.MessageDetailId});
+  },
+  getDateDiff:function(dateTime) {
+    let diffValue = dateTime;
+      let result = '';
+      let minute = 1000 * 60;
+      let hour = minute * 60;
+      let day = hour * 24;
+      let halfamonth = day * 15;
+      let month = day * 30;
+      let year = day * 365;
+      let now = new Date().getTime();
+      if(diffValue < 0) {
+        return diffValue;
+        }
+    let monthEnd = diffValue / month;
+      let weekEnd = diffValue / (7 * day);
+      let dayEnd = diffValue / day;
+      let hourEnd = diffValue / hour;
+      let minEnd = diffValue / minute;
+      let yearEnd = diffValue / year;
+      if(yearEnd >= 1) {
+    result = dateTime;
+    } else if (monthEnd >= 1) {
+      result = "" + parseInt(monthEnd) + "月前";
+      } else if (weekEnd >= 1) {
+      result = "" + parseInt(weekEnd) + "周前";
+      } else if (dayEnd >= 1) {
+      result = "" + parseInt(dayEnd) + "天前";
+      } else if (hourEnd >= 1) {
+      result = "" + parseInt(hourEnd) + "小时前";
+      } else if (minEnd >= 1) {
+      result = "" + parseInt(minEnd) + "分钟前";
+    } else {
+      result = "刚刚";
+    }
+    return result;
+    },
+    deleteNotice:function(){
+      Protocol.postNoticeUpdate().then(()=>{
+        this.postDynamicNotice();
+      });
+    },
+  
 
   /**
    * 生命周期函数--监听页面初次渲染完成
