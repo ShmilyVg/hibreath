9ad22f48cedb51c0504e922ba41018891989e8db
张浩玉 <826514335@qq.com>
2020-03-04 14:20:33 +0800

    Feat:我的减脂历程

---
+++ pages/low-carbon/low-carbon.js
@@ -0,0 +1,84 @@
+// pages/low-carbon/low-carbon.js
+import {getLatestOneWeekTimestamp} from "../../utils/time";
+import Protocol from "../../modules/network/protocol";
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    currenttab: 'howEatBox',
+  },
+  //切换标签页
+  async selectTab(e) {
+    let newtab = e.currentTarget.dataset.tabid;
+    console.log(newtab)
+    if (this.data.currenttab !== newtab) {
+      this.setData({
+        currenttab: newtab
+      });
+      console.log('currenttab',this.data.currenttab)
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  async onLoad(options) {
+    const {result} = await Protocol.getLossfatCourse()
+    this.setData({
+      result:result
+    })
+    console.log('result',result)
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
