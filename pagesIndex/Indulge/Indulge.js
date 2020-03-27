5a2e4a89a68ce023ffa0d8cb5a015a00a99242cc
张浩玉 <826514335@qq.com>
2020-03-27 17:34:27 +0800

    Feat:放纵餐怎么吃

--- pagesIndex/Indulge/Indulge.js
+++ pagesIndex/Indulge/Indulge.js
@@ -1,18 +1,32 @@
 // pagesIndex/Indulge/Indulge.js
+import HiNavigator from "../../navigator/hi-navigator";
+import Protocol from "../../modules/network/protocol";
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    foodList:[
+      {
+        "img": "http://img.hipee.cn/hibreath/food/%e7%94%9c%e8%84%86%e5%9d%9a%e6%9e%9c%e6%a3%92.png",
+        "calorie": "约420cal",
+        "carbohydrate": "约16.5g",
+        "quantityDesc":"3块/次",
+        "title":"拌小白菜",
+      }
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  async onLoad() {
+    const{result}= await Protocol.Indulge();
+    this.setData({
+      dataList:result
+    })
   },
 
   /**
