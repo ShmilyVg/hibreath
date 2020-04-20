8d1f7bd6b0d7a7cf8867d082a50493d4d8c78b3d
dangguojian <dangguojian@fruitech.cn>
2020-04-20 16:56:04 +0800

    Fix：新人7日礼修改跳转

--- pagesIndex/attendanceBonus/attendanceBonus.js
+++ pagesIndex/attendanceBonus/attendanceBonus.js
@@ -6,21 +6,21 @@ const app = getApp();
 Page({
 
   data: {
-    navBarShow:'Slimple轻松燃脂',
+    navBarShow: 'Slimple轻松燃脂',
     breathSign: {},//签到数据
   },
   onLoad: function (options) {
     this.getBreathSignInInfo();
-    
+
   },
 
   onShow: function () {
 
   },
-  async getGiftInfo(data){
-   
+  async getGiftInfo(data) {
+
     let couponCode = data.couponCode;
-    if (couponCode){
+    if (couponCode) {
       const { result } = await Protocol.getGift({ couponCode: couponCode })
       this.setData({
         couponCode,
@@ -28,38 +28,72 @@ Page({
       })
     }
   },
-  onPageScroll(ev){
-    if (!ev.scrollTop){
-      this.setData({
-        navBarShow:'Slimple轻松燃脂'
+  //函数节流 防止重复点击
+  debounce: function () {
+    var timeOut = null;
+    let that = this;
+    return (() => {
+      clearTimeout(timeOut);
+      timeOut = setTimeout(() => {
+        that.yzfn()
+      }, 300);
+    })();
+  },
+
+  //跳转有赞
+  async yzfn() {
+    wx.showToast({
+      title: '即将跳转有赞小程序',
+      icon: 'none',
+      duration: 1200
+    });
+    setTimeout(() => {
+      //跳转有赞小程序
+      wx.navigateToMiniProgram({
+        appId: 'wxeea809f23b5a5d9d',
+        extraData: {
+          // foo: 'bar'
+        },
+        envVersion: 'develop',
+        success: (res) => {
+          // 打开成功
+          console.log('打开有赞小程序成功', res);
+        }
       })
-    }else{
+    }, 1300)
+  },
+  onPageScroll(ev) {
+    if (!ev.scrollTop) {
+      this.setData({
+        navBarShow: 'Slimple轻松燃脂'
+      })
+    } else {
       this.setData({
         navBarShow: ''
       })
     }
   },
-  async takeGift(){
+  async takeGift() {
     let couponCode = this.data.couponCode;
     console.log(couponCode);
-    if (couponCode){
+    if (couponCode) {
       let res = await Protocol.takeGift({ couponCode });
-      if (res.code == 1){
+      if (res.code == 1) {
         wx.showToast({
           title: '领取成功,已放入“我的-优惠券”中',
           icon: "none",
           duration: 3000
         });
-        setTimeout(()=>{
+        setTimeout(() => {
           wx.navigateBack({
             delta: 1,
           })
-        },3000)
-        
+        }, 3000)
+
       }
     }
   },
-  handlerGobackClick(){
+  handlerGobackClick() {
     wx.navigateBack({
       delta: 1
     });
@@ -78,8 +112,8 @@ Page({
       breathSign: result
     })
   },
-  onShareAppMessage(){
-    return{
+  onShareAppMessage() {
+    return {
       title: '我在Slimple减脂中！拜托 拜托！来帮我补个卡吧~',
       path: `/pagesThree/help-cards/help-cards?sharedId=${this.data.breathSign.sharedId}`,
       imageUrl: this.data.breathSign.sharedImg
